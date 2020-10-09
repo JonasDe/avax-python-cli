5bd8bfa5578ca051a8ac456fe81f7ac404dbe9d7
Jonas De <jonasd90@gmail.com>
2020-10-08 20:34:16 -0700

    update: extract common logic

--- avax.py
+++ avax.py
@@ -1,31 +1,48 @@
 import requests
 import click
+import json
 
-HEADERS = { 'content-type': 'application/json;', }
+
+def query(endpoint, method, ip="http://127.0.0.1:9650", **kwargs):
+    HEADERS = { 'content-type': 'application/json;', }
+    DATA = f'{{ "jsonrpc":"2.0", "id" :1, "method" :"{method}", "params": {json.dumps(kwargs)} }}'
+    print(requests.post(**{"url":f"{ip}/{endpoint}", "headers":HEADERS, "data":DATA}).json())
+
+def add_options(options):
+    def _add_options(func):
+        for option in reversed(options):
+            func = option(func)
+        return func
+    return _add_options
+
+auth_options = [
+    click.option('--password', required=True, help='The node password.'),
+    click.option('--username', required=True, help='The node username.')
+    ]
 
 @click.group()
-def cli():
+def guest():
     pass
 
-@cli.command()
+@click.group()
+def auth():
+    pass
+
+
+@guest.command()
 def is_bootstrapped():
-    data = '{ "jsonrpc":"2.0", "id" :1, "method" :"info.isBootstrapped", "params": { "chain":"X" } }'
-    print(requests.post('http://127.0.0.1:9650/ext/info', headers=HEADERS, data=data).json())
+    query("ext/info", "info.isBootstrapped", chain="X")
 
-@click.option('--username', required=True, help='The node username.')
-@click.option('--password', required=True, help='The node password.')
-@cli.command()
+@auth.command()
+@add_options(auth_options)
 def create_user(username, password):
-    data = '{ "jsonrpc": "2.0", "id": 1, "method": "keystore.createUser", "params": { "username": "'+username+'", "password": "'+password+'" } }'
-    print(requests.post('http://127.0.0.1:9650/ext/keystore', headers=HEADERS, data=data).json())
+    query("ext/keystore", "keystore.createUser", username=username, password=password)
 
-@click.option('--username', required=True, help='The node username.')
-@click.option('--password', required=True, help='The node password.')
-@cli.command()
+@auth.command()
+@add_options(auth_options)
 def create_address(username, password):
-    data = '{ "jsonrpc":"2.0", "id" :2, "method" :"avm.createAddress", "params" :{ "username":"'+username+'", "password":"'+password+'" } }'
-    print(requests.post('http://127.0.0.1:9650/ext/bc/X', headers=HEADERS, data=data).json())
+    query("ext/bc/X", "avm.createAddress", username=username, password=password)
 
 if __name__ == '__main__':
+    cli = click.CommandCollection(sources=[auth, guest])
     cli()
-
